a411336f031fab0f7ffba479a7a5625deb4f59ba
Marco Taubmann <marco.taubmann@gmail.com>
2015-03-01 01:04:43 +0100

    Basic Snippeter
      * Snippeter with example application
    
      * Type ## in textarea to show snippet candidates,
        filter snippet candidates by typing words,
        select snippet with up/down keys,
        insert snippet with enter key or by click.
    
      * Highlight filter words (2+ characters) in
        snippet candidate list.

---
+++ app/snippeter.js
@@ -0,0 +1,213 @@
+function Snippeter (conf) {
+  this.snippets = [];
+  this.inputElements = null;
+  this.startTag = '#';
+  this.listElement = null;
+
+  if ('snippets' in conf) this.snippets = conf.snippets;
+  if ('inputElements' in conf) this.inputElements = conf.inputElements;
+  if ('startTag' in conf) this.startTag = conf.startTag;
+  if ('snippetToHtml' in conf) this.snippetToHtml = conf.snippetToHtml;
+  if ('listElement' in conf) this.listElement = conf.listElement;
+  if ('snippetMatcher' in conf) this.snippetMatcher = conf.snippetMatcher;
+  if ('highlighter' in conf) this.highligher(conf.highlighter);
+
+  this.init();
+
+}
+
+Snippeter.prototype.init = function init ()
+{
+  this.inputElements = $(this.inputElements);
+  this.listElement = $(this.listElement);
+
+  var that = this;
+
+  this.inputElements.keydown(function (event) {
+    that.keydown(event);
+  });
+
+  this.inputElements.keyup(function (event) {
+    that.keyup(event)
+  });
+}
+
+Snippeter.prototype.keydown = function keydown (event)
+{
+  var element = $(event.target);
+  var text = element.val();
+  var caretPos = element.caret();
+  var startTagPos = text.substring(0,caretPos).lastIndexOf(this.startTag);
+  if (startTagPos >= 0) {
+    //found open tag before caret
+    if ( event.keyCode === 13) { //enter
+      event.preventDefault();
+      this.insertSelected();
+    } else if (event.keyCode === 38) { //up
+      event.preventDefault();
+      this.selectionUp();
+    } else if (event.keyCode === 40) { //down
+      event.preventDefault();
+      this.selectionDown();
+    }
+  }
+}
+
+Snippeter.prototype.keyup = function keyup (event)
+{
+  var element = $(event.target);
+  var text = element.val();
+  var caretPos = element.caret();
+  var startTagPos = text.substring(0,caretPos).lastIndexOf(this.startTag);
+  if (startTagPos >= 0) {
+    //found open tag before caret
+    if ( -1 === $.inArray(event.keyCode, [13, 38, 40])) {
+      var searchText = text.substring(startTagPos + this.startTag.length, caretPos);
+      var snippets = this.filterSnippets(searchText);
+      this.updateList(snippets, searchText, element);
+    }
+  } else {
+    //clean list
+    this.updateList(null, null, element);
+  }
+};
+
+Snippeter.prototype.selectFirst = function selectFirst ()
+{
+  this.listElement.children('.selected').removeClass('selected');
+  this.listElement.children().first().addClass('selected');
+}
+
+Snippeter.prototype.selectLast = function selectLast ()
+{
+  this.listElement.children('.selected').removeClass('selected');
+  this.listElement.children().last().addClass('selected');
+}
+
+Snippeter.prototype.selectionUp = function selectionUp ()
+{
+  var selected = this.listElement.children('.selected');
+  selected.removeClass('selected');
+  if (selected.prev().length) {
+    selected.prev().addClass('selected');
+  } else {
+    this.selectLast();
+  }
+}
+
+Snippeter.prototype.selectionDown = function selectionDown ()
+{
+  var selected = this.listElement.children('.selected');
+  selected.removeClass('selected');
+  if (selected.next().length) {
+    selected.next().addClass('selected');
+  } else {
+    this.selectFirst();
+  }
+}
+
+Snippeter.prototype.insertSelected = function insertSelected ()
+{
+  this.listElement.children('.selected').trigger('click');
+}
+
+
+Snippeter.prototype.filterSnippets = function filterSnippets (searchText)
+{
+  console.log('filterSnippets: ', searchText);
+  if (searchText === '') {
+    return this.snippets;
+  } else {
+    var that = this;
+    var filtered = this.snippets.filter(function (snippet) {
+      return that.snippetMatcher(snippet, searchText);
+    });
+    return filtered;
+  }
+}
+
+Snippeter.prototype.updateList = function updateList (snippets, searchText, target)
+{
+  console.log('updateList: ', snippets);
+
+  var triggerUpdate = false;
+
+  if (this.listElement.children().length) {
+    triggerUpdate = true;
+  }
+  this.listElement.empty();
+
+  if (snippets && snippets.length) {
+    var that = this;
+    snippets.forEach(function (snippet) {
+      var snippetHtml = that.snippetToHtml(snippet);
+      snippetHtml = that.highlighter(snippetHtml, searchText);
+      var snippetElement = $(snippetHtml);
+      snippetElement.click(function () {
+        that.insert(snippet, target);
+      });
+      snippetElement.appendTo(that.listElement);
+    });
+
+    this.selectFirst();
+    triggerUpdate = true;
+  }
+
+  if (triggerUpdate) {
+    this.listElement.trigger({type: 'update', relatedTarget: target[0]});
+  }
+}
+
+Snippeter.prototype.insert = function insert (snippet, target)
+{
+  console.log('insert: ', snippet);
+
+  var text = target.val();
+  var caretPos = target.caret();
+  var startTagPos = text.substring(0,caretPos).lastIndexOf(this.startTag);
+
+  target.val(
+    text.substring(0, startTagPos)
+    + snippet
+    + text.substring(caretPos)
+  );
+
+  target.caret(startTagPos + snippet.length);
+
+  target.trigger('keyup');
+}
+
+Snippeter.prototype.snippetMatcher = function snippetMatcher (snippet, input) {
+  var words = input.split(' ');
+  var i;
+  for (i = 0; i < words.length; i++) {
+    if (words[i] !== ' ' && words[i] !== '') {
+      var regExp = new RegExp('(' + words[i] + ')', 'gi');
+      if (!snippet.match(regExp)) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+Snippeter.prototype.snippetToHtml = function snippetToHtml (snippet)
+{
+  return '<div>' + snippet + '</div>';
+};
+
+Snippeter.prototype.highlighter = function highlighter (source, highlight) {
+  if (highlight === '') {
+    return source;
+  }
+  var words = highlight.split(' ');
+  var i;
+  for (i = 0; i < words.length; i++) {
+    if (words[i].length > 1) { // don't highlight single characters to minimize highlighting html code :)
+      var regExp = new RegExp('(' + words[i] + ')', 'gi');
+      source = source.replace(regExp, '<span class="highlight">$1</span>');
+    }
+  }
+  return source;
+};
+
